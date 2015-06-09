636e262a578379ed09e6100cc7873cb9d1f3d6b2
taogogo <taogogo@163.com>
2015-06-09 18:37:51 +0800

    [add script]增加检查脚本

---
+++ ipdetect.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+netstat -anlp|grep 80|grep tcp|awk '{print $5}'|awk -F: '{print $1}'|sort|uniq -c|sort -nr|head -n20 | netstat -ant |awk '/:80/{split($5,ip,":");++A[ip[1]]}END{for(i in A) print A[i],i}' |sort -rn|head -n20 
+
+tcpdump -i eth0 -tnn dst port 80 -c 1000 | awk -F"." '{print $1"."$2"."$3"."$4}' | sort | uniq -c | sort -nr |head -20
